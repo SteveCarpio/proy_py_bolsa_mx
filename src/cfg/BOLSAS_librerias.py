740444fbef663eea071ae2b5e21dbe903cda9093
Steve Carpio <carpios@tda-sgft.com>
2025-03-07 13:08:42 +0100

    Update BOLSAS_librerias.py

--- src/cfg/BOLSAS_librerias.py
+++ src/cfg/BOLSAS_librerias.py
@@ -1,24 +1,23 @@
-# -------------------------------------------------------------------------------------------------
-#                      LIBRERÍAS NECESARIAS y OPCIONES
+# -------------------------------------------------------------------------------------
+#                      LIBRERÍAS NECESARIAS
 # Descripción: Abajo listo las librerías necesarias para su ejecución x pasos
 # Autor: SteveCarpio-2025
-# -------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------
 
-# CARGA DE LIBRERÍAS ------------------------------------------------------------------------------
-#                                                                 # Ma P0 P1 P2 P3 P4 P5 P6 P7 P8 #
-
-import os                                                         # ma -- -- -- -- -- -- -- -- -- #
-import time                                                       # -- -- -- -- -- -- -- -- -- -- #
-import glob                                                       # -- -- -- -- -- -- -- -- -- -- #
-import pandas as pd                                               # -- -- -- -- -- -- -- -- -- -- #
-import smtplib                                                    # -- -- -- -- -- -- -- -- -- -- # 
-import sys                                                        # -- -- -- -- -- -- -- -- -- -- #
-import subprocess                                                 # -- -- p1 p2 -- -- -- -- -- -- #
-import shutil                                                     # -- -- -- -- p3 -- -- -- -- -- #
-from colorama import init, Fore, Back, Style                      # -- -- -- -- -- -- -- -- -- -- #
-from datetime import datetime as dt                               # ma -- -- -- -- -- -- -- -- -- #
-from datetime import timedelta                                    # ma -- -- -- -- -- -- -- -- -- #
-from email.mime.multipart import MIMEMultipart                    # -- -- -- -- -- -- -- -- -- -- #
-from email.mime.text import MIMEText                              # -- -- -- -- -- -- -- -- -- -- #
-from email.mime.application import MIMEApplication                # -- -- -- -- -- -- -- -- -- -- #
-# -------------------------------------------------------------------------------------------------
+# CARGA DE LIBRERÍAS ------------------------------------------------------------------
+#                                                                 # Ma P0 P1 P2 P3 P4 #
+import os                                                         # ma p0 -- -- p3 p4 #
+import time                                                       # -- -- -- -- -- -- #
+import glob                                                       # -- p0 -- -- -- -- #
+import pandas as pd                                               # -- -- -- -- -- p4 #
+import smtplib                                                    # -- -- -- -- -- p4 # 
+import sys                                                        # -- -- -- -- -- -- #
+import subprocess                                                 # -- -- p1 p2 -- -- #
+import shutil                                                     # -- -- -- -- p3 -- #
+from colorama import init, Fore, Back, Style                      # ma p0 -- -- -- -- #
+from datetime import datetime as dt                               # ma -- -- -- -- -- #
+from datetime import timedelta                                    # ma -- -- -- -- -- #
+from email.mime.multipart import MIMEMultipart                    # -- -- -- -- -- p4 #
+from email.mime.text import MIMEText                              # -- -- -- -- -- p4 #
+from email.mime.application import MIMEApplication                # -- -- -- -- -- p4 #
+# -------------------------------------------------------------------------------------
