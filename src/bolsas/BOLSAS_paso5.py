d0cee0b564d0de41e9cba819d39b73c496fee474
Steve Carpio <carpios@tda-sgft.com>
2025-06-09 10:14:48 +0200

    compilando pq creaba un excel de bolsas vacio, no se tuvo en cuenta ese dato

--- src/bolsas/BOLSAS_paso5.py
+++ src/bolsas/BOLSAS_paso5.py
@@ -1,6 +1,20 @@
 # ----------------------------------------------------------------------------------------
 #  PASO5: ENVIÓ DE EMAIL
 #  Autor: SteveCarpio-2025
+#
+#  BOLSAS: Excel con los datos finales, datos preparados para que sean subidos a Oracle
+#  N	CLAVE	SECCION	   FECHA	    ASUNTO	             URL	                ARCHIVO	               ORIGEN	T	  FILTRO
+#  -------------------------------------------------------------------------------------------------------------------------------
+#  1	CAMSCB	Tenedores  2024-12-26	Aviso de Tenedores   https://www.bmv.com	https://www.bmv.com	   BMV	    P	  INCLUIR
+#  2	CAMSCB	Tenedores  2024-12-26   Aviso de Tenedores   https://www.biva.com   https://www.biva.com   BIVA	    M	  EXCLUIR
+#
+#  BMW: Excel que se manda en formato DF en un email  
+#	CLAVE	SECCION	  FECHA	       ASUNTO	             URL	                ARCHIVO                                   (email normal)
+#	CLAVE	SECCION	  FECHA	       ASUNTO	             URL	                ARCHIVO	                     EMAIL(P/M)   (email excluidos)
+#
+#  BIVA: Excel que se manda en formato DF en un email
+#	CLAVE	SECCION	  FECHA	       ASUNTO	             URL                                                              (email normal) 
+#	CLAVE	SECCION	  FECHA	       ASUNTO	             URL	                                             EMAIL(P/M)   (email excluidos)
 # ----------------------------------------------------------------------------------------
 
 import cfg.BOLSAS_variables as sTv
@@ -9,16 +23,6 @@ from   cfg.BOLSAS_librerias import *
 # ----------------------------------------------------------------------------------------
 #                               FUNCIONES DE APOYO
 # ----------------------------------------------------------------------------------------
-
-#N	CLAVE	SECCION	   FECHA	    ASUNTO	             URL	                ARCHIVO	             ORIGEN	 T	        FILTRO
-#1	CAMSCB	Tenedores  2024-12-26	Aviso de Tenedores   https://www.bmv.com	https://www.bmv.com	 BMV	 P	        INCLUIR
-#2	CAMSCB	Tenedores  2024-12-26	Aviso de Tenedores   https://www.bmv.com	https://www.bmv.com  BMV	 P	        INCLUIR
-# BMW  
-#	CLAVE	SECCION	  FECHA	       ASUNTO	             URL	                ARCHIVO
-#	CLAVE	SECCION	  FECHA	       ASUNTO	             URL	                ARCHIVO	                     EMAIL(P/M)
-# BIVA
-#	CLAVE	SECCION	  FECHA	       ASUNTO	             URL
-#	CLAVE	SECCION	  FECHA	       ASUNTO	             URL	                                             EMAIL(P/M)
 
 # Función Leer excel y convertirlos en DataFrame
 def sTv_paso5_lee_DF(ruta, bolsa, tipo):
@@ -30,9 +34,6 @@ def sTv_paso5_lee_DF(ruta, bolsa, tipo):
             columnas=['CLAVE','SECCION','FECHA','ASUNTO','URL','ARCHIVO','ORIGEN','T','FILTRO']
 
             # Convertir al tipo datetime y luego formatear
-            #df['FECHA'] = pd.to_datetime(df['FECHA'], format="%d-%m-%Y")
-            #df['FECHA'] = df['FECHA'].dt.strftime("%Y-%m-%d")
-
             df['FECHA'] = pd.to_datetime(df['FECHA'], format="%d-%m-%Y")
 
             # Formateamos cada DataFrame
@@ -83,7 +84,6 @@ def sTv_paso5(var_NombreSalida, var_Fechas2, var_Fechas3):
     ruta_excel6 = f"{sTv.var_RutaInforme}BMV_{var_Fechas3}_X.xlsx"
     ruta_excelT = f"{sTv.var_RutaInforme}BOLSAS_{var_Fechas3}.xlsx"
 
-    
     # Leer el archivo o crear el DataFrame por defecto
     df_BIVA_M = sTv_paso5_lee_DF(ruta_excel1, "BIVA"  ,"M")
     df_BIVA_P = sTv_paso5_lee_DF(ruta_excel2, "BIVA"  ,"P")
@@ -106,13 +106,17 @@ def sTv_paso5(var_NombreSalida, var_Fechas2, var_Fechas3):
     if isinstance(df_BMV_X, pd.DataFrame):
         df_TOTAL = pd.concat([df_TOTAL, df_BMV_X], ignore_index=True)
 
-    # Reasignar el índice para que empiece en 1
-    df_TOTAL.index = range(1, len(df_TOTAL) + 1)
+    if len(df_TOTAL) > 0:
 
-    # Cambio el nombre del índice a N en vez de que salga vació
-    df_TOTAL.index.name = "N"
+        # Reasignar el índice para que empiece en 1
+        df_TOTAL.index = range(1, len(df_TOTAL) + 1)
 
-    # Exportar el DataFrame a un Excel
-    df_TOTAL.to_excel(ruta_excelT,index=True,sheet_name="DATOS")
+        # Cambio el nombre del índice a N en vez de que salga vació
+        df_TOTAL.index.name = "N"
 
-    print(df_TOTAL)
+        # Exportar el DataFrame a un Excel
+        df_TOTAL.to_excel(ruta_excelT,index=True,sheet_name="DATOS")
+
+        print(df_TOTAL)
+    else:
+        print(f"No hay datos de BIVA o BMV para crear el excel de salida {ruta_excelT}")
--- src/cfg/BOLSAS_librerias.py
+++ src/cfg/BOLSAS_librerias.py
@@ -1,26 +1,26 @@
-# -------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
 #                      LIBRERÍAS NECESARIAS
-# Descripción: Abajo listo las librerías necesarias para su ejecución x pasos
+# Descripción: Lista de librerías distribuidas por pasos de ejecución
 # Autor: SteveCarpio-2025
-# -------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
 
-# CARGA DE LIBRERÍAS ------------------------------------------------------------------
-#                                                                 # Ma P0 P1 P2 P3 P4 #
-import io                                                         # ma -- -- -- -- -- #
-import re                                                         # ma -- -- -- -- -- #
-import os                                                         # ma p0 -- -- p3 p4 #
-import time                                                       # -- -- -- -- -- -- #
-import glob                                                       # -- p0 -- -- -- -- #
-import pandas as pd                                               # -- -- -- -- -- p4 #
-import smtplib                                                    # -- -- -- -- -- p4 # 
-import sys                                                        # -- -- -- -- -- -- #
-import subprocess                                                 # -- -- p1 p2 -- -- #
-import shutil                                                     # -- -- -- -- p3 -- #
-from colorama import init, Fore, Back, Style                      # ma p0 -- -- -- -- #
-from datetime import datetime as dt                               # ma -- -- -- -- -- #
-from datetime import timedelta                                    # ma -- -- -- -- -- #
-from email.mime.multipart import MIMEMultipart                    # -- -- -- -- -- p4 #
-from email.mime.text import MIMEText                              # -- -- -- -- -- p4 #
-from email.mime.application import MIMEApplication                # -- -- -- -- -- p4 #
-from bs4 import BeautifulSoup                                     # -- -- -- -- -- p4 #
-# -------------------------------------------------------------------------------------
+# CARGA DE LIBRERÍAS ---------------------------------------------------------------------
+#                                                                 # Ma P0 P1 P2 P3 P4 P5 #
+import io                                                         # ma -- -- -- -- -- -- #
+import re                                                         # ma -- -- -- -- -- -- #
+import os                                                         # ma p0 -- -- p3 p4 p5 #
+import time                                                       # -- -- -- -- -- -- -- #
+import glob                                                       # -- p0 -- -- -- -- -- #
+import pandas as pd                                               # -- -- -- -- -- p4 p5 #
+import smtplib                                                    # -- -- -- -- -- p4 -- #  
+import sys                                                        # -- -- -- -- -- -- -- #
+import subprocess                                                 # -- -- p1 p2 -- -- -- #
+import shutil                                                     # -- -- -- -- p3 -- -- #
+from colorama import init, Fore, Back, Style                      # ma p0 -- -- -- -- -- #
+from datetime import datetime as dt                               # ma -- -- -- -- -- p5 #
+from datetime import timedelta                                    # ma -- -- -- -- -- -- #
+from email.mime.multipart import MIMEMultipart                    # -- -- -- -- -- p4 -- #
+from email.mime.text import MIMEText                              # -- -- -- -- -- p4 -- #
+from email.mime.application import MIMEApplication                # -- -- -- -- -- p4 -- #
+from bs4 import BeautifulSoup                                     # -- -- -- -- -- p4 -- #
+# ----------------------------------------------------------------------------------------
