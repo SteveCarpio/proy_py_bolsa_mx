b0c8f2b438284bc536751a0114224f21e93d60b3
Steve Carpio <carpios@tda-sgft.com>
2025-06-16 17:23:41 +0200

    bash

---
+++ bash/BOLSAS.bat
@@ -0,0 +1,13 @@
+@echo off
+REM === CONFIGURACIÓN GENERAL ===
+set NOMBRE_JOB=BOLSAS_Main.exe
+set RUTA_JOB=C:\MisCompilados\PROY_BOLSA_MX
+set RUTA_LOG=%RUTA_JOB%\LOG
+
+REM === EJECUCIÓN DEL SCRIPT POWERSELL ===
+powershell.exe -ExecutionPolicy Bypass -Command ^
+"$fecha=(Get-Date).AddDays(-0).ToString('yyyy-MM-dd'); ^
+ $exe=Join-Path '%RUTA_JOB%' '%NOMBRE_JOB%'; ^
+ $logBase=Join-Path '%RUTA_LOG%' ([System.IO.Path]::GetFileNameWithoutExtension('%NOMBRE_JOB%') + '_' + $fecha); ^
+ Start-Process -FilePath $exe -ArgumentList 'RUN','PRO',$fecha -RedirectStandardOutput ($logBase + '_out.log') -RedirectStandardError ($logBase + '_err.log') -NoNewWindow -Wait"
+
