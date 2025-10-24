3a63b545a62615f915433f321cd38c5a4f9b0149
SteveCarpio <stv.madrid@gmail.com>
2025-10-24 13:48:31 +0200

    ORACLE inicio

---
+++ src_lnx/cfg/ORACLE_librerias.py
@@ -0,0 +1,21 @@
+# ----------------------------------------------------------------------------------
+#                      LIBRERÍAS NECESARIAS y OPCIONES
+# Descripción: Abajo listo las librerías necesarias para su ejecución x pasos
+# Autor: SteveCarpio-2025
+# ----------------------------------------------------------------------------------
+
+
+# CARGA DE LIBRERÍAS ---------------------------------------------------------------
+#                                                                 # Ma P0 P1 P2 P3 #
+import sys                                                        # ma -- -- -- -- #
+import re                                                         # ma -- -- -- -- #
+import os                                                         # ma p0 -- -- -- #
+import time                                                       # -- p0 -- -- -- #
+import glob                                                       # -- p0 -- -- -- #
+import pandas as pd                                               # -- -- p1 p2 -- #
+import pyodbc                                                     # -- -- -- p2 -- #
+from colorama import init, Fore, Back, Style                      # ma p0 p1 -- -- #
+from datetime import datetime as dt                               # ma -- -- -- -- #
+from datetime import timedelta                                    # ma -- -- -- -- #
+
+# ----------------------------------------------------------------------------------
---
+++ src_lnx/cfg/ORACLE_variables.py
@@ -0,0 +1,32 @@
+# ----------------------------------------------------------------------------------------
+#                                  VARIABLES DE APOYO
+# Descripción: Variables necesarias para la ejecución del proceso.
+# Autor: SteveCarpio-2025
+# ----------------------------------------------------------------------------------------
+import pandas as pd
+
+#-----------------------------------------------------------------------------------------
+#                          RUTAS DE APOYO
+# ----------------------------------------------------------------------------------------
+var_RutaRaiz='C:\\MisCompilados\\PROY_BOLSA_MX\\ORACLE\\'
+var_RutaInforme=f'{var_RutaRaiz}INFORMES\\'
+var_RutaConfig=f'{var_RutaRaiz}CONFIG\\'
+var_RutaLog=f'{var_RutaRaiz}LOG\\'
+var_RutaIN='C:\\MisCompilados\\PROY_BOLSA_MX\\INFORMES\\'
+
+# ----------------------------------------------------------------------------------------
+#                          VARIABLES DE APOYO
+# ----------------------------------------------------------------------------------------
+var_Ora_DNS="PYTHON_DATA"
+var_Ora_UID="PYDATA"
+var_Ora_PWD="PYDATA"
+var_Ora_TAB1="P_BOLSAS_EVENTOS_RELEVANTES"
+var_Ora_TAB2="P_CNBV_ESTADOS_FINANCIEROS"
+var_Files_IN="BOLSAS"
+df_Global = pd.DataFrame()
+
+# ----------------------------------------------------------------------------------------
+#                          AUTOR
+# ----------------------------------------------------------------------------------------
+var_sTv1="SteveCarpio-2025"
+var_sTv2="stv.madrid@gmail.com" 
