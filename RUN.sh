b639ed00b80f00abdaebbb78455b65e899b0ee79
SteveCarpio <stv.madrid@gmail.com>
2025-10-28 17:35:50 +0100

    CERRADO OK PRODUCCION

--- RUN.sh
+++ RUN.sh
@@ -22,10 +22,11 @@ JOB3="BOLSAS.sh"
 JOB4="ORACLE.sh"
 JOB5="EMISORES.sh"
 
-echo " "
+echo "-------------------------------------------------------------"
 echo "`date "+%Y-%m-%d %H.%M"`: CRON_BOLSAS INICIO"
+echo " "
 echo "`date "+%Y-%m-%d %H.%M"`: Ejecutando ${JOB1}"
-#${RUTA_RAIZ}${JOB1} $DIAS $ENTORNO
+${RUTA_RAIZ}${JOB1} $DIAS $ENTORNO
 echo "`date "+%Y-%m-%d %H.%M"`: ${JOB1} terminó correctamente"
 echo "`date "+%Y-%m-%d %H.%M"`: Ejecutando ${JOB2}"
 ${RUTA_RAIZ}${JOB2} $DIAS $ENTORNO
@@ -34,10 +35,11 @@ echo "`date "+%Y-%m-%d %H.%M"`: Ejecutando ${JOB3}"
 ${RUTA_RAIZ}${JOB3} $DIAS $ENTORNO
 echo "`date "+%Y-%m-%d %H.%M"`: ${JOB3} terminó correctamente"
 echo "`date "+%Y-%m-%d %H.%M"`: Ejecutando ${JOB4}"
-echo "${RUTA_RAIZ}${JOB4} $DIAS $ENTORNO "
+${RUTA_RAIZ}${JOB4} $DIAS $ENTORNO
 echo "`date "+%Y-%m-%d %H.%M"`: ${JOB4} terminó correctamente"
 echo "`date "+%Y-%m-%d %H.%M"`: Ejecutando ${JOB5}"
-echo "${RUTA_RAIZ}${JOB5} $DIAS $ENTORNO "
+${RUTA_RAIZ}${JOB5} $DIAS $ENTORNO
 echo "`date "+%Y-%m-%d %H.%M"`: ${JOB5} terminó correctamente"
+echo " "
 echo "`date "+%Y-%m-%d %H.%M"`: Todos los programas se ejecutaron con éxito."
 echo "`date "+%Y-%m-%d %H.%M"`: CRON_BOLSAS FIN"
